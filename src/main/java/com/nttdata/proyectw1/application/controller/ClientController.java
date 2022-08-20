b3c13dbb147b5d43d8de51cdcb2cc5d00c71f702
Luis Mendívil Bautista <lmendivi@emeal.nttdata.com>
2022-08-19 21:20:44 -0500

    Add ClientController

--- src/main/java/com/nttdata/proyectw1/application/controller/ClientController.java
+++ src/main/java/com/nttdata/proyectw1/application/controller/ClientController.java
@@ -1,4 +1,42 @@
 package com.nttdata.proyectw1.application.controller;
 
+import com.nttdata.proyectw1.domain.entity.Client;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping(name = "client")
 public class ClientController {
+
+    @PostMapping
+    public Mono<String> createClient(@RequestBody Client client){
+        return Mono.just("Successful Registration");
+    }
+
+    @PutMapping("/updateClient/{documentNumber}")
+    public Mono<String> updateClient(@RequestBody Client client, @RequestAttribute String documentNumber){
+        return Mono.just("Successful Update");
+    }
+
+    @GetMapping("/getClient/{documentNumber}")
+    public Mono<Client> getClient(@RequestAttribute String documentNumber){
+        return Mono.just(new Client());
+    }
+
+    @GetMapping("/getClients")
+    public Flux<Client> getClients(){
+        return Flux.just(new ArrayList<Client>().get(0));
+    }
+
+    @DeleteMapping("/deleteClient/{documentNumber}")
+    public Mono<String> deleteClient(@RequestAttribute String documentNumber){
+        return Mono.just("Successful Delete");
+    }
+
+
+
+
 }
