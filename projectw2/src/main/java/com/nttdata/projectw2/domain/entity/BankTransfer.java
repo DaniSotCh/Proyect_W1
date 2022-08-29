d6cdc97736d8246628d6f561d864253bdec494a5
Luis Mendívil Bautista <lmendivi@emeal.nttdata.com>
2022-08-29 15:23:57 -0500

    Add BankTransfer in projectw2

--- projectw2/src/main/java/com/nttdata/projectw2/domain/entity/BankTransfer.java
+++ projectw2/src/main/java/com/nttdata/projectw2/domain/entity/BankTransfer.java
@@ -1,4 +1,14 @@
 package com.nttdata.projectw2.domain.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class BankTransfer {
+
+    private String originAccountNumber;
+    private String targetAccountNumber;
+    private Double amount;
+
 }
