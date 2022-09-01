c80a25d186e44a23645a2921cb78acb0e6d1d383
Luis Mendívil Bautista <lmendivi@emeal.nttdata.com>
2022-09-01 09:06:49 -0500

    Update logic in method averageReport project2

--- projectw2/src/main/java/com/nttdata/projectw2/domain/service/ReportServiceImpl.java
+++ projectw2/src/main/java/com/nttdata/projectw2/domain/service/ReportServiceImpl.java
@@ -1,16 +1,15 @@
 package com.nttdata.projectw2.domain.service;
 
-import com.nttdata.projectw2.domain.entity.BankAccount;
-import com.nttdata.projectw2.domain.entity.Customer;
-import com.nttdata.projectw2.domain.entity.Passive;
-import com.nttdata.projectw2.domain.entity.Report;
+import com.nttdata.projectw2.domain.entity.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -34,23 +33,52 @@ public class ReportServiceImpl implements IReportService {
         Mono<BankAccount> passiveTransfer = null;
 
         List<Flux<BankAccount>> filterBA = new ArrayList<>();
+//        Map<String,Object> mapResult= new HashMap<>();
 
         customerMono.subscribe(x -> {
             Mono<BankAccount> response = null;
             System.out.println("-------------ENTRA2--------------");
-            for (Passive y : x.getPassiveList()) {
-                String accountNumber = y.getAccountNumber();
-                Flux<BankAccount> resp2 = this.webClient.get().uri("/bankAccount/getAllAmounts/{accountNumber}", accountNumber).retrieve().bodyToFlux(BankAccount.class);
-                System.out.println("-------------FILTRA--------------");
-                filterBA.add(resp2);
+//            mapResult.put("documentNumber",x.getDocumentNumber());
+            if(x.getPassiveList().size()>0){
+                for (Passive y : x.getPassiveList()) {
+//                    mapResult.put("accountNumber",y.getAccountNumber());
+                    String accountNumber = y.getAccountNumber();
+                    Flux<BankAccount> resp2 = this.webClient.get()
+                            .uri("/bankAccount/getAllAmounts/{accountNumber}", accountNumber)
+                            .retrieve()
+                            .bodyToFlux(BankAccount.class);
+                    resp2.subscribe(z->{
+//                        mapResult.put("z",z.toString());
+                        System.out.println("BankAccount passive--: "+z.getId());
+                    });
+                    System.out.println("-------------FILTRA PASSIVE--------------");
+                    filterBA.add(resp2);
+                }
             }
-            System.out.println("-------------SALE1--------------");
+
+            if(x.getActiveList().size()>0) {
+                for (Active z : x.getActiveList()) {
+                    String accountNumber2 = z.getAccountNumber();
+                    Flux<BankAccount> resp3 = this.webClient.get()
+                            .uri("/bankAccount/getAllAmounts/{accountNumber2}", accountNumber2)
+                            .retrieve()
+                            .bodyToFlux(BankAccount.class);
+                    resp3.subscribe(w -> {
+                        System.out.println("BankAccount active--: " + w.getId());
+                    });
+                    System.out.println("-------------FILTRA ACTIVE--------------");
+                    filterBA.add(resp3);
+                }
+            }
+//            System.out.println("mapResult1: "+mapResult.get("documentNumber"));
+
         });
 
-        System.out.println("-------------SALE2--------------");
+//        System.out.println("mapResult2: "+mapResult.get("documentNumber"));
 
         Report response = new Report();
 
         return Flux.just(response);
     }
+
 }
